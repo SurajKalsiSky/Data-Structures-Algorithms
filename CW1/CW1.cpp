a0ae3b44b3e5ce0f176a1cb642a767322f0a46a6
Suraj <Suraj.Kalsi@sky.uk>
2020-10-30 21:56:34 +0000

    finsihed hash table

--- CW1/CW1.cpp
+++ CW1/CW1.cpp
@@ -13,48 +13,46 @@ int main()
 {
     cout << "********************************************\n";
     cout << "Welcome to the C++ Redis database!\n";
-    cout << "You can use......\n";
-    cout << "Finish a command by\n";
-    cout << "Finish all commands with EXEC\n";
-    cout << "Example usage........\n";
-    cout << "Please enter your commands\n";
+    cout << "You can use 'GET' and 'SET' commands\n";
+    cout << "Finish a command using '-'\n";
+    cout << "Finish program with EXEC\n";
     cout << "********************************************\n\n";
 
-    Queue queue(3);
+    Queue queue(5); // initalise queue with a size of 5
 
-    bool userInsertingCommands = true;
-    V redisCommand;
+    bool userInsertingCommands = true; // boolean used to exit while loop
+    V redisCommand; // initialise vector to store Redis command
 
-    while (userInsertingCommands) {
-        string stringCommand;
+    while (userInsertingCommands) { // loop to obtain all user inputs
+        string stringCommand; // initialise string for user input
 
         cout << "Enter command, finsh by tapping enter: ";
-        cin >> stringCommand;
+        cin >> stringCommand; // user input is stored to stringCommand variable
 
-        if (stringCommand == "-") {
-            userInsertingCommands = queue.Insert(redisCommand);
-            redisCommand.erase(redisCommand.begin(), redisCommand.end());
+        if (stringCommand == "-") { // check if user has finished entering command
+            userInsertingCommands = queue.Insert(redisCommand); // insert vector containing command into the queue. If queue is full, exit loop
+            redisCommand.erase(redisCommand.begin(), redisCommand.end()); // flush the vector to enter new command
         }
-        else if (stringCommand == "EXEC") {
-            userInsertingCommands = false;
+        else if (stringCommand == "EXEC") { // check if user is done with program and wants to display output
+            userInsertingCommands = false; // set boolean to exit loop
         }
         else {
-            redisCommand.push_back(stringCommand);
+            redisCommand.push_back(stringCommand); // insert string into vector as it is a valid command
         }
     }
 
-    RedisHashTable* table = new RedisHashTable(17);
+    RedisHashTable* table = new RedisHashTable(17); // initialise hash table with a size that is a prime number
 
-    while (true) {
-        V element = queue.Remove();
+    while (true) { // loop through all elements in the queue
+        V element = queue.Remove(); // on each iteration, remove the first element from the queue
 
-        if (element[0] == "end") {
+        if (element[0] == "end") { // once end of queue is reached, exit loop
             break;
         }
 
-        table->Insert(10, &element);
+        table->Insert(19, &element); // insert each element's reference into the hash table
     }
 
-    table->Display();
+    table->Display(); // invoke hash table's Display function
 }
 
--- CW1/RedisHashTable.h
+++ CW1/RedisHashTable.h
@@ -9,31 +9,36 @@ using V = vector<string>; // alias for a vector string
 
 class RedisHashTable {
 private:
-	V** data;
-	int size;
-	int HashFunction(int key) {
-		return key % size;
+	V** data; // pointer to a pointer for data attribute of a vector type
+	int size; // size of the hash table
+	int HashFunction(int key) { // function to hash key to find a free index
+		return size - (key % size); // double hashing algorithm
 	}
 public:
 	RedisHashTable(int size) {
-		this->size = size;
-		data = new V* [size];
+		this->size = size; // initialise size (class) variable
+		data = new V* [size]; // initialise hash table with correct size
 		for (int i = 0; i < size; i++)
-			data[i] = NULL;
+			data[i] = NULL; // assign each element inside hash table a value of NULL
 	}
 	void Display() {
-		for (int i = 0; i < size; i++)
-			if (data[i] != NULL)
-				cout << "Index: " << i << " Contents: " << data[i] << endl;
+		cout << '\n';
+		for (int i = 0; i < size; i++) // iterate over all elements in hash table
+			if (data[i] != NULL) { // true only when element has been set
+				cout << "Index: " << i << " Pointer reference: " << data[i] << endl; // prints the index and reference of the vector
+			}
 	}
-	void Insert(int key, V* item) {
-		int hash = HashFunction(key);
-		while (data[hash] != NULL) {
-			++hash;
-			hash %= size;
+	void Insert(int key, V* item) { // function takes key and a pointer to the vector being stored
+		cout << '\n';
+		int hash = HashFunction(key); // get index value via hash function
+		while (data[hash] != NULL) { // if there already is a value at that index, find a new slot
+			++hash; // increment hash value
+			hash = HashFunction(hash); // get new index value via hash function
 		}
-		data[hash] = item;
-
+		data[hash] = item; // set reference value in hash table
+		V redisCommand = *item; // get actual vector from pointer reference
+		for (string ele : redisCommand) // loop through elements in the vector
+			cout << "Set: " << ele << endl; // print each element
 	}
 };
 
